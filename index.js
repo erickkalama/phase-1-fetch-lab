15eddacbaad9a7d9a07aa63d98bb7343a1376a23
GHOSTFACE <erickkatana4787@gmail.com>
2024-07-14 15:42:55 +0300

    completed Fetch Lab (CodeGrade))

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => renderBooks(data))
+    .catch((error) => console.error('Error fetching books:', error));
+}
+function renderBooks(books) {
+  const bookList = document.getElementById('book-list');
+
+  books.forEach((book) => {
+    const li = document.createElement('li');
+    li.textContent = book.name;
+    bookList.appendChild(li);
+  });
+}
+document.addEventListener('DOMContentLoaded', () => {
+  fetchBooks();
+});
+
